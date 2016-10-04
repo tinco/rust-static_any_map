909025e6146137938d43fd01b2f715a990358662
Tinco Andringa <mail@tinco.nl>
2016-10-05 01:11:49 +0200

    start static_any_map checkin

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+#[macro_export]
+macro_rules! static_any_vec_map {
+	($v:ident, $t:ty) => {
+		pub mod $v {
+			use std::collections::{ HashMap };
+			use std::iter::FromIterator;
+			use std::any::Any;
+			use shared_mutex::{ SharedMutex, MappedSharedMutexReadGuard };
+
+			lazy_static! {
+				pub static ref MAP: SharedMutex<HashMap<$t, Box<Any+'static+Sync>>> = SharedMutex::new(HashMap::new());
+			}
+
+			pub fn push<T>(event_name: & $t, event: T) where T: Any+'static+Sync {
+				let mut map = MAP.write().expect("Static MAP is not initialized.");
+				let mut entry = map.entry(event_name.clone()).or_insert(Box::new(Vec::<T>::new()));
+				let mut casted_entry = &mut **entry as &mut Any;
+				let mut vec = casted_entry.downcast_mut::<Vec<T>>().expect("Could not cast created entry to Vec<T>");
+				vec.push(event);
+			}
+
+			pub fn get<T>(event_name: & $t) -> MappedSharedMutexReadGuard<Vec<T>> where T: Any+'static+Sync {
+				let map = MAP.read().expect("Static MAP is not initialized.");
+				let vec = map.into_mapped().map(|m| {
+					let entry = m.get(event_name).expect("Could not get a particular MAP entry.");
+					let casted_entry = & **entry as & Any;
+					return casted_entry.downcast_ref::<Vec<T>>().expect("Could not cast gotten entry to Vec<T>");
+				});
+				return vec;
+			}
+
+			pub fn clear<T>(event_name: & $t) where T: Any+'static+Sync {
+				let mut map = MAP.write().expect("Static MAP is not initialized.");
+				let mut entry = map.entry(event_name.clone()).or_insert(Box::new(Vec::<T>::new()));
+				let mut casted_entry = &mut **entry as &mut Any;
+				let mut vec = casted_entry.downcast_mut::<Vec<T>>().expect("Could not cast created entry to Vec<T>");
+				vec.clear();
+			}
+
+			pub fn drain<'a>() -> Vec<($t, Box<Any+'static+Sync>)> {
+				let mut map = MAP.write().expect("Static MAP is not initialized.");
+				return Vec::from_iter(map.drain());
+			}
+
+			pub fn clear_all() {
+				let mut map = MAP.write().expect("Static MAP is not initialized.");
+				map.clear();
+			}
+		}
+	}
+}
---
+++ tests/static_any_map.rs
@@ -0,0 +1,68 @@
+#[macro_use]
+extern crate static_any_map;
+#[macro_use]
+extern crate lazy_static;
+extern crate shared_mutex;
+
+use std::collections::HashMap;
+
+static_any_vec_map! { my_map, String }
+
+#[test]
+fn static_map_initial_set() {
+	let i : i64 = 5;
+	let name = String::from("my_queue");
+	my_map::clear_all();
+	my_map::push(&name, i);
+	let queue = my_map::get(&name);
+	assert_eq!(queue.len(), 1);
+	assert_eq!(i, queue[0]);
+}
+
+#[test]
+fn static_map_multi_set() {
+	let i : i64 = 5;
+	let j : i64 = 352;
+	let k : i64 = 234;
+	let name = String::from("my_queue");
+	my_map::clear_all();
+	my_map::push(&name, i);
+	my_map::push(&name, j);
+	my_map::push(&name, k);
+	let queue = my_map::get(&name);
+	assert!(queue.len() == 3);
+	assert!(i == queue[0]);
+	assert!(j == queue[1]);
+	assert!(k == queue[2]);
+}
+
+#[test]
+fn static_map_multi_queue_set() {
+	let i : i64 = 5;
+	let j : i64 = 352;
+	let name = String::from("my_queue");
+	let name2 = String::from("next_queue");
+	my_map::clear_all();
+	my_map::push(&name, i);
+	my_map::push(&name2, j);
+	{
+		let queue = my_map::get(&name);
+		assert_eq!(queue.len(), 1);
+		assert_eq!(i, queue[0]);
+	}
+	{
+		let queue2 = my_map::get(&name2);
+		assert_eq!(queue2.len(), 1);
+		assert_eq!(j, queue2[0]);
+	}
+}
+
+#[test]
+fn test_how_string_works_as_hashmap_index() {
+	let name = String::from("my_queue");
+	let mut m : HashMap<String, i64> = HashMap::new();
+	let i = 543;
+	m.insert(name.clone(), i);
+	let j = *m.get(&name.clone()).unwrap();
+	assert!(i == j);
+}
